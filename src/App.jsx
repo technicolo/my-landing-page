7de053cead580d6095dc039fe5287c42452adfff
technicolo <franfrare19@gmail.com>
2025-05-10 18:57:36 -0300

    intetnto 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-
 import { useState, useEffect } from "react";
 import Loader from "../public/components/resources/loader/loader";
 import Projects from "../public/components/projects/project";
 import Profile from "../public/components/profile/profile";
 import Inicio from "../public/components/Hero/inicio";
 import ProjectDetail from "../public/components/projects/prjectDetail";
-
-
 
 function AppContent() {
   const location = useLocation();
@@ -17,7 +14,7 @@ function AppContent() {
     setLoading(true);
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 500); // tiempo de la animación (800ms)
+    }, 500);
     return () => clearTimeout(timer);
   }, [location]);
 
@@ -25,28 +22,21 @@ function AppContent() {
     window.scrollTo(0, 0);
   }, [location]);
 
-
   return (
     <>
-    
       {loading && <Loader />}
-     
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/projects/:id" element={<ProjectDetail />} />
-        {/* más rutas si querés */}
       </Routes>
-    
     </>
   );
 }
 
 function App() {
-  
   return (
-    
     <Router>
       <AppContent />
     </Router>
